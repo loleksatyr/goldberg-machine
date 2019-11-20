479018647cb7f7ac007ef4d8151a324299eb6707
loleksatyr <czarnotabartek@gmail.com>
2019-11-20 16:58:41 +0100

    goldman12

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+  public Transform Spherefollow;
+  private Vector3 _cameraOffset;
+  [Range(0.01f,1.0f)]
+  public float SmoothFactor= 0.5f;
+  public bool lksp;
+    void Start()
+    {
+        _cameraOffset = transform.position - Spherefollow.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = Spherefollow.position + _cameraOffset; 
+        transform.position = Vector3.Slerp(transform.position,newPos,SmoothFactor);
+    if(lksp){
+        transform.LookAt(Spherefollow);
+    }
+    }
+}
